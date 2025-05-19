6a2731508f092c3946c9df05c2585f0011fb9968
Mahesh Behere <maheshbehere14@gmail.com>
2025-05-19 10:50:55 +0530

    Initial Commit

---
+++ OnlineGroceryApp/OnlineGroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineGroceryAppApp.swift
+//  OnlineGroceryApp
+//
+//  Created by Mahesh Behere on 19/05/25.
+//
+
+import SwiftUI
+
+@main
+struct OnlineGroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
